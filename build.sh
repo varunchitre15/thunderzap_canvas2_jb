3ea7187f78507e5e409e349c336649ddcaf267c1
Varun <varun.chitre15@gmail.com>
2013-09-20 14:32:20 +0100

    Initial commit

---
+++ build.sh
@@ -0,0 +1,1 @@
+make -j12 ARCH=arm CROSS_COMPILE=~/A70_kernel/toolchain/arm-linux-androideabi-4.6/bin/arm-linux-androideabi- TARGET_PRODUCT=s9081 MTK_ROOT_CUSTOM=../mediatek/custom/
---
+++ clean.sh
@@ -0,0 +1,1 @@
+make -j12 ARCH=arm CROSS_COMPILE=~/A70_kernel/toolchain/arm-eabi-linaro-4.6.2/bin/arm-eabi- TARGET_PRODUCT=s9081 MTK_ROOT_CUSTOM=../mediatek/custom clean mrproper
